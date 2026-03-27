1fc6fbf07d457ee1f0c5e42ef1a85aa61e8707db
Chanchal Choudhary <chanchalchoudhary986@gmail.com>
2026-03-27 23:36:29 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" },
-  [React.createElement("div", { id: "child1" },
-    [React.createElement("h1", { id: "heading1" }, "I am h1 Tag"),
-     React.createElement("h2", { id: "heading2" }, "I am h2 Tag")
+  [React.createElement("div", { id: "child1" , key: "C1"},
+    [React.createElement("h1", { id: "heading1", key: "c1-h1" }, "I am h1 Tag"),
+     React.createElement("h2", { id: "heading2", key: "c1-h2" }, "I am h2 Tag")
     ]
   ),
-  React.createElement("div", { id: "child2" },
-    [React.createElement("h1", { id: "heading1" }, "I am h1 Tag"),
-     React.createElement("h2", { id: "heading2" }, "I am h2 Tag")
+  React.createElement("div", { id: "child2", key: "C2" },
+    [React.createElement("h1", { id: "heading1", key: "c2-h1" }, "I am h1 Tag"),
+     React.createElement("h2", { id: "heading2", key: "c2-h2" }, "I am h2 Tag")
     ]
   )]
 );
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namaste React
+# Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optmization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older version
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Dev & Production Bundles
